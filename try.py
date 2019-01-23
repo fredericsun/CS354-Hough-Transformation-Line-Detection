da9305dd7f2d288911bb9cae2807a9144de1f4e5
Yue Sun <ysun276@wisc.edu>
2019-01-22 22:47:29 -0600

    Update try.py

--- try.py
+++ try.py
@@ -1,14 +1,10 @@
-class Handoff:
-    def __init__(self, robot, groupid, microid, larm_token, rarm_token, lhand_token, rhand_token, side, give_receive):
-        self.robot = robot
+class Question:
+    def __init__(self, robot, groupid, microid, speech_token, AnswerList):
         self.groupid = groupid
         self.microid = microid
-        self.larm_token = larm_token
-        self.rarm_token = rarm_token
-        self.lhand_token = lhand_token
-        self.rhand_token = rhand_token
-        self.side = side
-        self.give_receive = give_receive
+        self.robot = robot
+        self.speech_token = speech_token
+        self.AnswerList = AnswerList
 
     def getGroup(self):
         return self.groupid
@@ -17,146 +13,80 @@ class Handoff:
         return self.microid
 
     def execute(self, last_final_state, out_list):
-        wait_time = 15
-        StartState = "Start_Present_Arm_Retracted"
-        if self.side == None:
-            self.side = "right"
+        StartState = "Start_Silent_Present_H_Silent"
+        wait_time = 5
+
         while True:
-            if StartState == "Start_Present_Arm_Retracted":
-                self.robot.gaze.addBehavior("Handoff", "GAZE_AT", None)
+            if StartState == "Start_Silent_Present_H_Silent":
+                self.robot.gaze.addBehavior("Ask", "GAZE_AT")
                 print StartState + "\n"
-                '''
-                if self.side == "left":
-                    self.larm_token.acquire()
-                    self.robot.leftarmRetracted()
-                    self.larm_token.release()
-                    self.lhand_token.acquire()
-                    self.robot.closeLefthand()
-                    self.lhand_token.release()
-                elif self.side == "right":
-                    self.rarm_token.acquire()
-                    self.robot.rightarmRetracted()
-                    self.rarm_token.release()
-                    self.rhand_token.acquire()
-                    self.robot.closeRighthand()
-                    self.rhand_token.release()
-                '''
-                newState = "Present_Extending_Arm_Extended"
-                self.robot.gaze.killBehavior("Handoff", "GAZE_AT")
+                newState = "Speaking_Present_H_Silent_Asking"
                 print newState + "\n"
+                self.robot.gaze.killBehavior("Ask", "GAZE_AT")
 
-            if newState == "Present_Extending_Arm_Extended":
-                if self.side == "left":
-                    self.robot.gaze.addBehavior("Handoff", "GAZE_REFERENTIAL", "left")
-                    self.larm_token.acquire()
-                    self.robot.leftarmExtended()
-                    self.larm_token.release()
-                    touch = self.robot.waitUntilTouchDetected()
-                    print touch
-                    self.robot.gaze.killBehavior("Handoff", "GAZE_REFERENTIAL")
-                elif self.side == "right":
-                    self.robot.gaze.addBehavior("Handoff", "GAZE_REFERENTIAL", "right")
-                    self.rarm_token.acquire()
-                    self.robot.rightarmExtended()
-                    self.rarm_token.release()
-                    touch = self.robot.waitUntilTouchDetected()
-                    print touch
-                    self.robot.gaze.killBehavior("Handoff", "GAZE_REFERENTIAL")
-                newState = "Releasing_Contacted_Arm_Extended"
-                print newState + "\n"
+            if newState == "Speaking_Present_H_Silent_Asking":
+                self.robot.gaze.addBehavior("Ask", "GAZE_AT")
+                self.robot.gesture.addBehavior("Ask", "GESTURE_BEAT")
+                self.speech_token.acquire()
+                self.robot.question()
+                self.speech_token.release()
+                newState = "Silent_Present_Listening_H_Silent"
+                self.robot.gaze.killBehavior("Ask", "GAZE_AT")
+                self.robot.gesture.killBehavior("Ask", "GESTURE_BEAT")
 
-            if self.give_receive == "receive":
-                if newState == "Releasing_Contacted_Arm_Extended":
-                    if self.side == "left":
-                        self.robot.gaze.addBehavior("Handoff", "GAZE_AT", None)
-                        self.lhand_token.acquire()
-                        self.robot.openLefthand()
-                        self.lhand_token.release()
-                        self.robot.gaze.killBehavior("Handoff", "GAZE_AT")
-                    elif self.side == "right":
-                        self.robot.gaze.addBehavior("Handoff", "GAZE_AT", None)
-                        self.rhand_token.acquire()
-                        self.robot.openRighthand()
-                        self.rhand_token.release()
-                        self.robot.gaze.killBehavior("Handoff", "GAZE_AT")
-                    newState = "Releasing_Present_Arm_Extended"
+            while True:
+                if newState == "Silent_Present_Listening_H_Silent":
+                    self.robot.gaze.addBehavior("Ask", "GAZE_AT")
+                    self.robot.gesture.addBehavior("Ask", "GESTURE_NONE")
                     print newState + "\n"
+                    val = self.robot.speechRecognition(wait_time)
+                    if val is None or val == "":
+                        newState = "Silent_Ignored_H_Silent_End"
+                        print newState + "\n"
+                        self.robot.gaze.killBehavior("Ask", "GAZE_AT")
+                        self.robot.gesture.killBehavior("Ask", "GESTURE_NONE")
+                        break
+                    else:
+                        newState = "Silent_Listening_H_Speaking_Answering"
+                        self.robot.gaze.killBehavior("Ask", "GAZE_AT")
+                        #self.robot.gesture.killBehavior("Ask", "GESTURE_NONE")
+                        print newState + "\n"
 
-                if newState == "Releasing_Present_Arm_Extended":
-                    touch = self.robot.waitUntilTouchDetected()
-                    print touch
-                    if self.side == "left":
-                        if touch[4][1] == True:
-                            self.robot.gaze.addBehavior("Handoff", "GAZE_AT", None)
-                            self.lhand_token.acquire()
-                            self.robot.closeLefthand()
-                            self.lhand_token.release()
-                            time.sleep(1)
-                            self.larm_token.acquire()
-                            self.robot.leftarmRetracted()
-                            self.larm_token.release()
-                            self.robot.gaze.killBehavior("Handoff", "GAZE_AT")
-                            newState = "Present_End_Arm_Retracted"
+                    if newState == "Silent_Listening_H_Speaking_Answering":
+                        self.robot.gaze.addBehavior("Ask", "GAZE_AT")
+                        #self.robot.gesture.addBehavior("Ask", "GESTURE_NONE")
+                        if val in self.AnswerList:
+                            newState = "Silent_Present_Listening_H_Silent"
                             print newState + "\n"
-                            output = "human_ready"
-                            time.sleep(2)
-                            break
-                    elif self.side == "right":
-                        if touch[1][1] == True:
-                            self.robot.gaze.addBehavior("Handoff", "GAZE_AT", None)
-                            self.rhand_token.acquire()
-                            self.robot.closeRighthand()
-                            self.rhand_token.release()
-                            time.sleep(1)
-                            self.rarm_token.acquire()
-                            self.robot.rightarmRetracted()
-                            self.rarm_token.release()
-                            self.robot.gaze.killBehavior("Handoff", "GAZE_AT")
-                            newState = "Present_End_Arm_Retracted"
+                            newState = "Silent_Present_H_Silent_End"
                             print newState + "\n"
-                            output = "human_ready"
-                            time.sleep(2)
+                            self.robot.gaze.killBehavior("Ask", "GAZE_AT")
+                            #self.robot.gesture.killBehavior("Ask", "GESTURE_NONE")
                             break
+                        elif val not in self.AnswerList:
+                            newState = "Silent_Present_Listening_H_Silent"
+                            self.robot.gaze.killBehavior("Ask", "GAZE_AT")
+                            #self.robot.gesture.killBehavior("Ask", "GESTURE_NONE")
+                            print newState + "\n"
+                            newState = "Speaking_RepeatAsk_Present_H_Silent"
+                            print newState + "\n"
+                            self.robot.gaze.addBehavior("Ask", "GAZE_AT")
+                            self.robot.gesture.addBehavior("Ask", "GESTURE_BEAT")
+                            self.robot.repeatAnswer()
+                            newState = "Silent_Present_Listening_H_Silent"
+                            self.robot.gaze.killBehavior("Ask", "GAZE_AT")
+                            self.robot.gesture.killBehavior("Ask", "GESTURE_BEAT")
 
-            if self.give_receive == "give":
-                if newState == "Releasing_Contacted_Arm_Extended":
-                    if self.side == "left":
-                        self.robot.gaze.addBehavior("Handoff", "GAZE_AT", None)
-                        self.lhand_token.acquire()
-                        self.robot.openLefthand()
-                        self.robot.closeLeftHand()
-                        self.lhand_token.release()
-                        self.robot.gaze.killBehavior("Handoff", "GAZE_AT")
-                    elif self.side == "right":
-                        self.robot.gaze.addBehavior("Handoff", "GAZE_AT", None)
-                        self.rhand_token.acquire()
-                        print "OPENING RIGHT HAND"
-                        self.robot.openRighthand()
-                        print "CLOSING RIGHT HAND"
-                        self.robot.closeRighthand()
-                        self.rhand_token.release()
-                        self.robot.gaze.killBehavior("Handoff", "GAZE_AT")
-                    newState = "Releasing_Present_Arm_Extended"
-                    print newState + "\n"
+            if newState == "Silent_Ignored_H_Silent_End":
+                output = "human_ignore"
+                break
 
-                if newState == "Releasing_Present_Arm_Extended":
-                    #time.sleep(5)
-                    if self.side == "left":
-                        self.robot.gaze.addBehavior("Handoff", "GAZE_AT", None)
-                        print "ABOUT TO RETRACT ARM!"
-                        self.larm_token.acquire()
-                        self.robot.leftarmRetracted()
-                        self.larm_token.release()
-                        self.robot.gaze.killBehavior("Handoff", "GAZE_AT")
-                    elif self.side == "right":
-                        self.robot.gaze.addBehavior("Handoff", "GAZE_AT", None)
-                        print "ABOUT TO RETRACT ARM!"
-                        self.rarm_token.acquire()
-                        self.robot.rightarmRetracted()
-                        self.rarm_token.release()
-                        self.robot.gaze.killBehavior("Handoff", "GAZE_AT")
-                    newState = "Present_End_Arm_Retracted"
-                    print newState + "\n"
+            if newState == "Silent_Present_H_Silent_End":
+                idx = AnswerList.index(val)
+                link = linkList[idx]
+                if (link == "human_ready" or link == ""):
                     output = "human_ready"
-                    break
+                else:
+                    output = "human_ignore"
+                break
         out_list.append(output)
